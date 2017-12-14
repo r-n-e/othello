ec57c11c1d88aa42d82bea6c0d9f63d6d1d05700
r-n-e <34546350+r-n-e@users.noreply.github.com>
2017-12-15 01:05:01 +0900

    Update othello.c

--- othello.c
+++ othello.c
@@ -100,6 +100,15 @@ bool existEnableCells(int enableCells[64][2]) {
 
 bool fillBoard(Cell board[HIGHT][WIDTH]) {
   // TODO: ボードが埋まっていたらtrue
+  /*for (int y=0; y < HIGHT; y++) {
+    for(int x=0; x < WIDTH ; x++) {
+      if(board[x][y] == Blank) {
+        return false;
+      }else if(y == HIGHT-1 && x == WIDTH-1) {
+        return true;
+      }
+    }
+  }*/
   return true;
 }
 
